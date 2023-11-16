a501c673b64f879f4511f16f3fc4820b456e83c7
Argenis De La Rosa <oftherose91@icloud.com>
2023-11-16 12:04:44 -0500

    displayed the articles in the list page

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,7 +1,19 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 const ArticleListPage = () => {
 
     return (
-        <h1>This is the article list page!</h1>
+        <>
+            <h1>Articles</h1>
+            {
+                articles.map(article => (
+                    <Link to={'/articles/${article.name}'}>
+                        <h3>{article.title}</h3>
+                        <p>{article.content[0].substring(0, 150)}...</p>
+                    </Link>
+                ))}
+        </>
     );
 }
 
