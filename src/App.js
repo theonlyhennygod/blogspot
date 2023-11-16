d4cb5df95d773a158c5e3d48881212f30b91182e
Argenis De La Rosa <oftherose91@icloud.com>
2023-11-15 19:22:07 -0500

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ArticleListPage from './pages/ArticleList';
 import ArticlePage from './pages/ArticlePage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+
 
 function App() {
   return (
-    <div className="App">
-
-      <h1>My Blog Spot</h1>
-      <div id='page__body'>
-        <h1>Welcome to my blog!</h1>
-        <HomePage />
-        <AboutPage />
-        <ArticleListPage />
-        <ArticlePage />
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Blog Spot</h1>
+        <div id='page__body'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles' element={<ArticleListPage />} />
+            <Route path='/articles/:articlesId' element={<ArticlePage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
