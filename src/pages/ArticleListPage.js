1a3fa6b7b9c15022a2a53dca0e1eda95032372d8
Argenis De La Rosa <theonlyhennygod@gmail.com>
2023-11-18 16:25:13 -0500

    created a component for the articles list

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,17 +1,12 @@
 import articles from "./article-content";
-import { Link } from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
 
 const ArticleListPage = () => {
 
     return (
         <>
             <h1>Articles</h1>
-            {articles.map(article => (
-                <Link key={article.name} className="article-list-item" to={'/articles'}>
-                    <h3>{article.title}</h3>
-                    <p>{article.content[0].substring(0, 150)}...</p>
-                </Link>
-            ))}
+            <ArticlesList articles={articles} />
         </>
     );
 }
